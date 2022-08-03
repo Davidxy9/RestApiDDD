deefd6ef51353c9ee819ae2f2f4a41bb90e76c99
David <david.lima@meta.com.br>
2022-08-03 14:15:18 -0300

    Inicial Project

---
+++ RestApiDDD.Domain.Core/Interfaces/Repositorys/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiDDD.Domain.Core.Interfaces.Repositorys
+{
+    public interface IRepositoryBase
+    {
+    }
+}
